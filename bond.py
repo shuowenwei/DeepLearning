3927150b695cfb737c9d8cea80f66387b02e12f3
Wei, Shuowen <Shuowen.Wei@finra.org>
2018-08-08 19:47:14 -0400

    add two methods

--- bond.py
+++ bond.py
@@ -25,7 +25,7 @@ plt.hist(np.random.chisquare(15,3500))
 issuer_bond_dict = dict() 
 totalNumBonds = 0 
 for issuer, numBond in zip(issuers, np.random.chisquare(15,3500)): 
-    issuer_bond_dict[issuer] = [issuer+str(int(numBond)) for i in range(int(numBond))] 
+    issuer_bond_dict[issuer] = [issuer+str(i) for i in range(int(numBond))] 
     totalNumBonds += int(numBond) 
 
 print("Total number of bonds: ", totalNumBonds) 
@@ -47,32 +47,91 @@ for i in range(totalTransactions):
     transactionsList.append([getABond(), getATime(), getAPrice()])
 
 columnNames = ['BondName', 'Time', 'Price']
-pd_transactions = pd.DataFrame(transactionsList, columns=columnNames, index=range(totalTransactions))
-pd_transactions.head()
-
-###############################################################################
+df_trsctn = pd.DataFrame(transactionsList, columns=columnNames, index=range(totalTransactions))
 bond_issuedby_dict = dict()
 for issuer, bondList in issuer_bond_dict.items():
     for bond in bondList:
         bond_issuedby_dict[bond] = issuer
         
-pd_transactions['issuer'] = pd_transactions['BondName'].apply(lambda bondname: bond_issuedby_dict[bondname])
-pd_transactions.head()
+df_trsctn['issuer'] = df_trsctn['BondName'].apply(lambda bondname: bond_issuedby_dict[bondname])
 
-pd_transactions[pd_transactions['Time'] < 4.7].groupby(['issuer', 'BondName'])['Price'].max() 
+df_trsctn_timeSorted = df_trsctn.sort_values('Time').reset_index(drop=True).copy(deep=True)
+df_trsctn_timeSorted.head()
+
+#df_trsctn_timeSorted = df_trsctn_timeSorted.set_index(['BondName'])
+#df_trsctn_timeSorted.head()
+#def getLastTransactionTime(strBondName, floatTime):
+#    return df_trsctn_timeSorted[(df_trsctn_timeSorted['BondName'] == strBondName)
+#                            & (df_trsctn_timeSorted['Time'] < floatTime)
+#                        ]['Time'].max()
+#
+#df_trsctn_timeSorted['LastTime'] = df_trsctn_timeSorted.apply(lambda row: getLastTransactionTime(row['BondName'], row['Time']), axis = 1)
+#getLastTransactionTime('wGhy1', 0.000003)
+
+df_trsctn_timeSorted['LastTime'] = df_trsctn_timeSorted.groupby('BondName').Time.shift(1).fillna(float(0))
+#df_trsctn_timeSorted[df_trsctn_timeSorted['BondName'] == 'wGhy1']
+df_trsctn_timeSorted.head()
+
+###############################################################################
+"""
+df_trsctn[df_trsctn['Time'] < 4.7].groupby(['issuer', 'BondName'])['Price'].max() 
+
+issuer = 'RcAJ'
+df_trsctn[df_trsctn['Time'] < 4.7].groupby(['issuer', 'BondName'])['Price'].max()
+df_trsctn[(df_trsctn['issuer'] == issuer) & (df_trsctn['Time'] < 4.7) ].sort_values('Time') 
+
+df = df_trsctn[(df_trsctn['issuer'] == issuer)
+          & (df_trsctn['Time'] < 4.7)
+          & (df_trsctn['BondName'] != 'anything')].groupby(['BondName']).head(1)[['BondName','Price']]
+"""
+###############################################################################
 
 
-issuer = ''
-pd_transactions[pd_transactions['Time'] < 4.7].groupby(['issuer', 'BondName'])['Price'].max()
-pd_transactions[pd_transactions['issuer'] = issuer].groupby(['issuer', 'BondName'])['Price'].max()
+df_train = df_trsctn_timeSorted[df_trsctn_timeSorted['Time'] > 1].reset_index(drop=True).copy(deep=True)
+df_train.head() 
+len(df_train)
+#df_train2 = df_trsctn_timeSorted[df_trsctn_timeSorted['Time'] > 1].sort_values('Time').reset_index(drop=True).copy(deep=True)
+#df_train.equals(df_train2) 
+#for i, j in zip(df_train.index, df_train2.index):
+#    if df_train.iloc[i]['BondName'] != df_train2.iloc[j]['BondName'] or df_train.iloc[i]['Price'] != df_train2.iloc[j]['Price'] or df_train.iloc[i]['Time'] != df_train2.iloc[j]['Time']:
+#        print(i)
+
+########## method 1
+n = 0 
+df_train['targetBondList2'] = " "
+for index, trac in df_train.iterrows():
+    n += 1 
+    print("currently processing n = ", n)
+    df = df_trsctn_timeSorted[(df_trsctn_timeSorted['issuer'] == trac['issuer'])
+                                & (df_trsctn_timeSorted['Time'] >= trac['LastTime'])
+                                & (df_trsctn_timeSorted['Time'] < trac['Time'])
+                                & (df_trsctn_timeSorted['BondName'] != trac['BondName'])
+                                ].groupby(['BondName']).tail(1)
+    df_train.at[index, 'targetBondList2'] = df.values.tolist()
+
+
+########## method 2
+def getBondbySameIssuer(strBondName, floatTime, strIssuer, floatLastTime):
+    return df_trsctn_timeSorted[(df_trsctn_timeSorted['issuer'] == strIssuer)
+                                & (df_trsctn_timeSorted['Time'] >= floatLastTime)
+                                & (df_trsctn_timeSorted['Time'] < floatTime)
+                                & (df_trsctn_timeSorted['BondName'] != strBondName)
+                                ].groupby(['BondName']).tail(1).values.tolist()
+
+df_train['targetBondList'] = df_train.apply(lambda row: getBondbySameIssuer(row['BondName'], row['Time'], row['issuer'], row['LastTime']), axis = 1)
+#   BondName      Time  Price   issuer
+#0  GTMlIfW1  4.999993     44  GTMlIfW
+#1    TkddJ2  5.000000     28    TkddJ
+#a = getBondbySameIssuer('GTMlIfW1', 4.999993, 'GTMlIfW').values.flatten().tolist()
+#len(a)
+#getBondbySameIssuer('TkddJ2', 5.000000, 'TkddJ').values.tolist()
+
+df_train.iloc[0]['targetBondList']
+df_train.iloc[0]['targetBondList2']
 
 
 
 
-
-
-
-
-
-
+#df_train.head()
+#df_train.drop(['new1'], axis=1, inplace=True)
 
